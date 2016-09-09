c2b7b947d82ecb96c00149da473a22d6543e7fa3
Nicolas Hurtado <nickh93@outlook.com>
2016-09-09 14:39:26 -0400

    create logic that will display the information entered by the user on the home page using twig

--- app/app.php
+++ app/app.php
@@ -14,14 +14,22 @@
     ));
 
     $app->get('/', function() use ($app) {
-        return $app['twig']->render('contacts.html.twig', array('contact' => Contact::getAll()));
+        return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
     });
 
     $app->post('/create_contact', function() use ($app) {
-        $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
+        $contact = new Contact($_POST['name'], $_POST['number'], $_POST['address']);
         $contact->save();
-        return $app['twig']->render('create_contact.html.twig', array('new-contact' => $contact));
-    })
+        return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
+    });
+
+    $app->post('/delete', function() use ($app) {
+
+        Contact::deleteAll();
+
+        return $app['twig']->render('delete_places.html.twig');
+
+    });
 
     return $app;
 ?>
